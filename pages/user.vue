b4c4a8ef86e7c0d3557328625ce7b654c4b73606
iKoru <glassonyou@gmail.com>
2018-12-05 23:31:30 +0900

    implementing user management page - in progress(2)

--- pages/user.vue
+++ pages/user.vue
@@ -8,23 +8,22 @@
         </v-toolbar>
         <v-form>
           <v-select v-model="searchTarget" :items="searchTargetItems" label="검색 대상"></v-select>
-          <v-input-field v-model="searchQuery"></v-input-field>
-          <v-btn primary flat @click="getDataFromApi">검색</v-btn>
+          <v-input-field name="searchQuery" v-model="searchQuery"></v-input-field>
+          <v-btn primary @click="getDataFromApi">검색</v-btn>
         </v-form>
-        <v-data-table :headers="headers" :items="items" id="userTable" :rows-per-page-items="[10]" :loading="loading" :total-items="totalUsers" :pagination.sync="pagination">
+        <v-data-table :headers="headers" :items="users" id="userTable" :rows-per-page-items="[10]" :loading="loading" :total-items="totalUsers" :pagination.sync="pagination">
           <template slot="items" slot-scope="props">
-            <td>{{ props.item.name }}</td>
-            <td class="text-xs-right">{{ props.item.calories }}</td>
-            <td class="text-xs-right">{{ props.item.fat }}</td>
-            <td class="text-xs-right">{{ props.item.carbs }}</td>
-            <td class="text-xs-right">{{ props.item.protein }}</td>
+            <td>{{ props.item.userId }}</td>
+            <td class="text-xs-right">{{ props.item.loungeNickName }}</td>
+            <td class="text-xs-right">{{ props.item.topicNickName }}</td>
+            <td class="text-xs-right">{{ userStatusItems.find(x=>x.value === props.item.status).text }}</td>
             <td class="justify-center layout px-0">
               <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
               <v-icon small @click="deleteItem(props.item)">delete</v-icon>
             </td>
           </template>
           <template slot="no-data">
-            <v-btn color="primary" @click="initialize">Reset</v-btn>
+            <v-btn color="primary" @click="getDataFromApi">Reset</v-btn>
           </template>
           <template slot="actions-prepend">
             <v-spacer></v-spacer>
@@ -34,29 +33,26 @@
                 <v-card-title>
                   <span class="headline">{{ formTitle }}</span>
                 </v-card-title>
-      
+
                 <v-card-text>
                   <v-container grid-list-md>
                     <v-layout wrap>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                        <v-text-field name="userId" v-model="editedItem.userId" label="ID"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                        <v-text-field name="loungeNickName" v-model="editedItem.loungeNickName" label="라운지 닉네임"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                        <v-text-field name="topicNickName" v-model="editedItem.topicNickName" label="토픽 필명"></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                        <v-select name="status" v-model="editedItem.status" :items="userStatusItems" label="상태"></v-select>
                       </v-flex>
                     </v-layout>
                   </v-container>
                 </v-card-text>
-      
+
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
@@ -64,6 +60,7 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
+            <v-spacer></v-spacer>
           </template>
         </v-data-table>
       </v-layout>
@@ -74,115 +71,95 @@
 <script>
 // import PlainTable from "~/components/PlainTable";
 // import PlainTableOrder from "~/components/PlainTableOrder";
-import config from '~/assets/js/config'
+import config from "~/assets/js/config";
 export default {
   data: () => ({
-      dialog: false,
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Actions', value: 'name', sortable: false }
-      ],
-      items: [],
-      editedIndex: -1,
-      editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
-      },
-      loading:true,
-      pagination:{},
-      searchTargetItems:[
-        {text:'이메일', value:'email'},
-        {text:'ID', value:'userId'},
-        {text:'닉네임', value:'nickName'},
-        {text:'상태', value:'status'}
-        ],
-      searchQuery:null,
-      searchTarget:'userId'
+    dialog: false,
+    headers: [{text: "ID", align: "left", value: "userId"}, {text: "라운지닉네임", value: "loungeNickName"}, {text: "토픽필명", value: "topicNickName"}, {text: "상태", sortable: false, value: "status"}, {text: "수정/삭제", value: "actions", sortable: false}],
+    users: [],
+    totalUsers: 0,
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    },
+    loading: true,
+    pagination: {},
+    searchTargetItems: [{text: "이메일", value: "email"}, {text: "ID", value: "userId"}, {text: "닉네임", value: "nickName"}, {text: "상태", value: "status"}],
+    userStatusItems: [{text: "인증전", value: "NORMAL"}, {text: "인증완료", value: "AUTHORIZED"}, {text: "잠김", value: "BLOCKED"}, {text: "삭제처리", value: "DELETED"}],
+    searchQuery: null,
+    searchTarget: "userId"
   }),
 
   computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? '회원 생성' : '회원 수정'
+    formTitle() {
+      return this.editedIndex === -1 ? "회원 생성" : "회원 수정";
     }
   },
 
   watch: {
-    dialog (val) {
-      val || this.close()
+    dialog(val) {
+      val || this.close();
     }
   },
 
-  mounted : async function() {
-    let data = await this.getDataFromApi()
-    this.items = data.items
-    this.totalUsers = data.totalUsers
+  mounted: function() {
+    this.getDataFromApi();
   },
 
   methods: {
-    getDataFromApi : async function (){
-      this.loading = true
-      const { sortBy, descending, page, rowsPerPage } = this.pagination
+    getDataFromApi: async function() {
+      this.loading = true;
+      const {sortBy, descending, page, rowsPerPage} = this.pagination;
       let query = {
-        sortTarget:sortBy,
-        isAscending:!descending,
-        page:page
+        sortTarget: sortBy,
+        isAscending: !descending,
+        page: page
       };
-      if(this.searchQuery){
+      if (this.searchQuery) {
         query[this.searchTarget] = this.searchQuery;
       }
-      let items = await this.$axios.get( `${config.apiServerHost}/user/list`, query)
-      console.log(items);
-      const total = items.length
-
-      if (this.pagination.sortBy) {
-        items = items.sort((a, b) => {
-          const sortA = a[sortBy]
-          const sortB = b[sortBy]
-
-          if (descending) {
-            if (sortA < sortB) return 1
-            if (sortA > sortB) return -1
-            return 0
-          } else {
-            if (sortA < sortB) return -1
-            if (sortA > sortB) return 1
-            return 0
-          }
-        })
+      let response = await this.$axios.get(`${config.apiServerHost}/user/list`, query);
+      console.log(response);
+      if (response.status !== 200) {
+        this.loading = false;
+        return;
       }
+      let items = response.data;
+      const total = items.length;
+      // if (this.pagination.sortBy) {
+      //   items = items.sort((a, b) => {
+      //     const sortA = a[sortBy];
+      //     const sortB = b[sortBy];
+
+      //     if (descending) {
+      //       if (sortA < sortB) return 1;
+      //       if (sortA > sortB) return -1;
+      //       return 0;
+      //     } else {
+      //       if (sortA < sortB) return -1;
+      //       if (sortA > sortB) return 1;
+      //       return 0;
+      //     }
+      //   });
+      // }
 
       if (rowsPerPage > 0) {
-        items = items.slice((page - 1) * rowsPerPage, page * rowsPerPage)
+        items = items.slice((page - 1) * rowsPerPage, page * rowsPerPage);
       }
-      return new Promise((resolve, reject) => {
-
-        setTimeout(() => {
-          this.loading = false
-          resolve({
-            items,
-            total
-          })
-        }, 1000)
-      })
+      this.users = items;
+      this.totalUsers = total;
+      this.loading = false;
     },
     /*getDesserts () {
       return [
@@ -279,45 +256,45 @@ export default {
       ]
     },*/
 
-    editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+    editItem(item) {
+      this.editedIndex = this.users.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
 
-    deleteItem (item) {
-      const index = this.desserts.indexOf(item)
-      confirm('정말 이 회원을 삭제하시겠습니까? 해당 회원이 작성한 글 등은 유지되고, 소유한 토픽이 있을 경우 삭제가 불가능합니다.') && this.desserts.splice(index, 1)
+    deleteItem(item) {
+      const index = this.users.indexOf(item);
+      confirm("정말 이 회원을 삭제하시겠습니까? 해당 회원이 작성한 글 등은 유지되고, 소유한 토픽이 있을 경우 삭제가 불가능합니다.") && this.desserts.splice(index, 1);
     },
 
-    close () {
-      this.dialog = false
+    close() {
+      this.dialog = false;
       setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      }, 300)
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
     },
 
-    save () {
+    save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        Object.assign(this.users[this.editedIndex], this.editedItem);
       } else {
-        this.desserts.push(this.editedItem)
+        this.users.push(this.editedItem);
       }
-      this.close()
+      this.close();
     }
   },
-  layout:'main',
-  head(){
+  layout: "main",
+  head() {
     return {
-      title: '회원 관리'
-    }
+      title: "회원 관리"
+    };
   }
 };
 </script>
 
 <style lang="styl">
-  #userTable{
-    width:100%;
-  }
+#userTable {
+  width: 100%;
+}
 </style>
