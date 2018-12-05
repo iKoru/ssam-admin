72da4c1cbdd482b8c6f70ea6ce3ad4377dedbdbf
koru <glassonyou@gmail.com>
2018-12-05 00:50:17 +0000

    generated menu files

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,49 @@
 const Menu = [
-  { header: 'Apps' },
+  { header: 'Pedagy Admin Main' },
   {
-    title: 'Dashboard',
-    group: 'apps',
+    title: '메인',
+    group: 'main',
     icon: 'dashboard',
     name: 'index',
   },
-
+  {header: '회원'},
+  {
+    title: '회원 관리',
+    group: 'user',
+    icon: 'person',
+    name: 'user',
+  },
+  {
+    title: '그룹 관리',
+    group: 'user',
+    icon: 'group',
+    name: 'group',
+  },
+  {header: '게시판'},
+  {
+    title:'게시판 관리',
+    group:'board',
+    icon:'folder',
+    name:'board'
+  },
+  {
+    title:'신고종류 관리',
+    group:'board',
+    icon:'flag',
+    name:'reportType'
+  },
+  {
+    title:'게시물 신고 관리',
+    group:'board',
+    icon:'report',
+    name:'report'
+  },
+  {
+    title:'동물명 관리',
+    group:'board',
+    icon:'style',
+    name:'animal'
+  }
 ];
 // reorder menu
 Menu.forEach((item) => {
