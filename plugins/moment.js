af9436786527eacb5825893541e2b7083ab9ec6b
iKoru <glassonyou@gmail.com>
2018-12-08 01:24:19 +0900

    prevent duplicate assignment issue

--- plugins/moment.js
+++ plugins/moment.js
@@ -2,4 +2,4 @@ import Vue from 'vue';
 import moment from 'moment';
 
 //Vue.prototype.$moment = moment;
-Object.defineProperty(Vue.prototype, '$moment', { value: moment });
+if (!Vue.prototype.$moment) Object.defineProperty(Vue.prototype, '$moment', { value: moment });
